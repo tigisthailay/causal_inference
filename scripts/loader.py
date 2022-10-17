a8d509db7e21c5ac1ba27e562fd288c9f8e01f06
Tegisty Hailay <tigisthay13@gmail.com>
2022-10-17 16:53:48 +0300

    loading dataset

--- scripts/loader.py
+++ scripts/loader.py
@@ -1,33 +1,22 @@
 
 import json
 import sys
-#from git import Object
+from git import Object
 import pandas as pd
-#from logger import Logger
-
 
 class LoadData:
 
 
     def __init__(self) -> None:
-        """Initilize class."""
-        """try:
-            self.logger = Logger("logger.log").get_app_logger()
-            self.logger.info("Successfully Instantiated load_data Class Object")
-        except Exception:
-            self.logger.exception("Failed to Instantiate load_data Class Object")
-            sys.exit(1)
-            """
-
+    """
     def read_json(self, json_file: str, dfExtractFunc: Object) -> pd.DataFrame:
-
         data_list = []
         for item in open(json_file, "r"):
             data_list.append(json.loads(item))
 
         df = dfExtractFunc(data_list)
         return df
-
+    """
     def read_excel(self, excel_file, startRow=0) -> pd.DataFrame:
         
         df = pd.read_excel(excel_file, engine="openpyxl")
